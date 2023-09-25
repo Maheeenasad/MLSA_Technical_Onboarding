d4df10f0a0fc759a759d4076557834dbadba6556
Maheen Asad <107026918+Maheeenasad@users.noreply.github.com>
2023-09-25 23:14:51 +0500

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Portfolio Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "My portfolio website showcases my creative journey and professional achievements.",
+    url: "https://sma-portfolio.vercel.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Online Banking Website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "My banking website, built with React.js, offers a user-friendly and efficient way to manage finances.",
+    url: "https://banking-web-lime.vercel.app/",
   },
   {
-    title: "My Resume Site",
+    title: "Travel and Tours Website",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "My travel and tours website invites you to explore the world with ease and excitement.",
+    url: "https://maheeenasad.github.io/TravelandTours-Responsive-Website/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Coffee Shop Website",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "My coffee shop website captures the essence of every cup, inviting you to savor the perfect brew.",
+    url: "https://maheeenasad.github.io/CentralPerk/",
   },
 ];
 
