79bf5195e1518f8164bdc286a526fa1bfcf6a2bd
Maheen Asad <107026918+Maheeenasad@users.noreply.github.com>
2023-09-25 23:08:56 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Maheen Asad",
+  title: "Website Designer & Developer",
+  email: "maheenasad19@gmail.com",
+  gitHub: "maheeenasad",
+  instagram: "maheeenasad",
+  linkedIn: "Maheen Asad",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "maheeenasad",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
